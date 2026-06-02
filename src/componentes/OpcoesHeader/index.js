ab3f03f01a7b661b84a5297b3b6f720eea3fd6cb
ntorracca <ntorracca@gmail.com>
2026-06-02 17:29:01 -0300

    feat: :sparkles: add: feat/aula02/novos componentes

---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,14 @@
+import '../OpcoesHeader/estilo.css';
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
+function OpcoesHeader (){
+    return (
+        <ul className='opcoes'>
+        { textoOpcoes.map( (texto) => (
+          <li className='opcao'><p>{texto}</p></li>))
+        }
+        </ul>
+    )
+}
+
+export default OpcoesHeader;
