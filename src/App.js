3859666bb0b97399f28530b04a82278db221daef
ntorracca <ntorracca@gmail.com>
2026-06-04 17:23:27 -0300

    feat: :sparkles: add: aula05/props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './componentes/Header'
 import Pesquisa from './componentes/Pesquisa'
+import UltimosLancamentos from './componentes/UltimosLancamentos'
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
@@ -7,14 +8,13 @@ const AppContainer = styled.div`
    height: 100vh;
    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
 `
-
 function App() {
  return (
    <AppContainer>
      <Header />
      <Pesquisa />
+     <UltimosLancamentos />
    </AppContainer>
  );
 }
-
 export default App
