bca58a4536a3a7a73dc7228c8a99f6a4073e9bf2
ntorracca <ntorracca@gmail.com>
2026-06-28 12:32:09 -0300

    feat: :sparkles:

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
@@ -22,11 +23,11 @@ const Opcoes = styled.ul
 
 function OpcoesHeader (){
     return (
-        <Opcoes>
-        { textoOpcoes.map( (texto) => (
-          <Opcao><p>{texto}</p></Opcao>))
-        }
-        </Opcoes>
+       <Opcoes>
+           { textoOpcoes.map( (texto) => (
+               <Link to={`/${texto}`} ><Opcao><p>{texto}</p></Opcao></Link>
+           ) ) }
+     </Opcoes>
     )
 }
 
