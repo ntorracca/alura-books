02efe7727cf11ab3d0534ef21b9ff68b7a551370
ntorracca <50490400+ntorracca@users.noreply.github.com>
2026-06-29 16:18:22 -0300

    Revert "feat: :sparkles: add: aula02/parte2 - Requisições com Axios"

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,10 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
-import { getLivros } from '../../servicos/livros'
-import { useEffect } from 'react'
-
-
+import { livros } from './dadosPesquisa'
 const PesquisaContainer = styled.section`
    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
    color: #FFF;
@@ -13,23 +10,17 @@ const PesquisaContainer = styled.section`
    height: 470px;
    width: 100%;
 `
-
-
 const Titulo = styled.h2`
    color: #FFF;
    font-size: 36px;
    text-align: center;
    width: 100%;
 `
-
-
 const Subtitulo = styled.h3`
    font-size: 16px;
    font-weight: 500;
    margin-bottom: 40px;
 `
-
-
 const Resultado = styled.div`
    display: flex;
    justify-content: center;
@@ -46,45 +37,28 @@ const Resultado = styled.div`
        border: 1px solid white;
    }
 `
-
-
 function Pesquisa() {
    const [livrosPesquisados, setLivrosPesquisados] = useState([])
-   const [livros, setLivros] = useState([])
-
-
-   useEffect(() => {
-       fetchLivros()
-   }, [])
-
-
-   async function fetchLivros() {
-       const livrosDaAPI = await getLivros()
-       setLivros(livrosDaAPI)
+   function fazPesquisa(evento) {
+       const textoDigitado = evento.target.value
+       const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+       setLivrosPesquisados(resultadoPesquisa)
    }
-
-
-   return (
+     return (
        <PesquisaContainer>
            <Titulo>Já sabe por onde começar?</Titulo>
            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
            <Input
                placeholder="Escreva sua próxima leitura"
-               onBlur={evento => {
-                   const textoDigitado = evento.target.value
-                   const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
-                   setLivrosPesquisados(resultadoPesquisa)
-               }}
+               onBlur={evento => fazPesquisa(evento)}
            />
            { livrosPesquisados.map( livro => (
                <Resultado>
-                   <img src={livro.src}/>
+                   <img src={livro.src} alt='livro'/>
                    <p>{livro.nome}</p>
                </Resultado>
            ) ) }
        </PesquisaContainer>
    )
 }
-
-
 export default Pesquisa
